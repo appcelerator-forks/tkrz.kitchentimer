ba8eeab7aad6cc265b3484472be694fb33ef58fb
tkrz <tomasz.j.krzywda@gmail.com>
2014-12-10 17:10:49 +0100

    Working on side menu.

--- app/widgets/sideMenu/controllers/widget.js
+++ app/widgets/sideMenu/controllers/widget.js
@@ -1,12 +1,12 @@
 $.open = function(){
 	$.sideMenu.open({
-	    activityEnterAnimation: Ti.Android.R.anim.fade_in
+	    activityEnterAnimation: Ti.Android.R.anim.none
     });
 };
 
 $.close = function(){
 	// $.bg.animate({backgroundColor: '#00000000', duration: 200},function(){
-		$.sideMenu.close({activityExitAnimation: Ti.Android.R.anim.fade_out});
+		$.sideMenu.close({activityExitAnimation: Ti.Android.R.anim.none});
 	// });
 };
 
@@ -14,3 +14,13 @@ function animate(){
 	$.bg.animate({backgroundColor: '#80000000', duration: 200});
 }
 
+function animateMenu(e){
+    Ti.API.info('Touch move');
+    var left = e.x - 200;
+    if(left <= 0)
+        $.menu.animate({left: left, duration: 30});
+}
+
+function menuSettle(e){
+    Ti.API.info(e.x);
+}
